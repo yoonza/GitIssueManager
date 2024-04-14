b9a115c86f95677796b3994e36ec84ab848a73f5
YOONZA <68805229+yoonza@users.noreply.github.com>
2024-04-14 23:25:51 +0900

    Update DetailPage.jsx
    - 리스트화면 / 상세화면 연동 오류 해결
    - 본문(body)에 Markdown style 추가 
    - 모바일/웹 화면 크기 자동 조정

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -1,11 +1,12 @@
-//DetailPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import Header from '../Components/Header';
 import ReactMarkdown from 'react-markdown';
+import { useParams } from 'react-router-dom'; // useParams를 import합니다.
 
-const DetailPage = ({ match }) => {
+const DetailPage = () => {
+  const { issueNumber } = useParams(); // URL 파라미터에서 issueNumber를 가져옵니다.
   const [issue, setIssue] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -13,7 +14,6 @@ const DetailPage = ({ match }) => {
     const fetchIssueDetails = async () => {
       setLoading(true);
       try {
-        const issueNumber = match.params.issueNumber;
         const response = await axios.get(
           `https://api.github.com/repos/angular/angular-cli/issues/${issueNumber}`
         );
@@ -25,7 +25,7 @@ const DetailPage = ({ match }) => {
     };
   
     fetchIssueDetails();
-  }, [match.params.issueNumber]);
+  }, [issueNumber]); // 이제 issueNumber를 의존성 배열에 추가합니다.
 
   return (
     <Container>
@@ -51,7 +51,8 @@ export default DetailPage;
 const Container = styled.div`
   font-family: Arial, sans-serif;
   padding: 20px;
-  width: 500px;
+  width: 100%; /* 모바일 앱 화면일 때 전체 너비를 차지하도록 설정 */
+  max-width: 80%; /* 웹 화면일 때 최대 너비를 80%로 제한 */
   border: 3px solid black;
   margin: 20px auto;
   height: 800px;
@@ -80,4 +81,4 @@ const Avatar = styled.img`
 
 const StyledMarkdown = styled(ReactMarkdown)`
   font-size: 14px; /* 원하는 폰트 크기로 조절하세요 */
-`;
+`; 
